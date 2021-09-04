8e10804d6093127bfe924a834d3d69b10a9a294f
Marvin <captainpismire@gmail.com>
2021-09-03 21:44:24 -0700

    removed temporary stuff in rom!

--- src/ROM.py
+++ src/ROM.py
@@ -158,10 +158,6 @@ class ROM:
     def getBaseDir(self):
         fileNames = list(filter(lambda key: self.isPatched[key], self.data.keys()))
         comDir = os.path.dirname(os.path.commonprefix(fileNames))
-        ### TEMPORARY ###
-        tmp = comDir.split('/')[1:]
-        tmp = ['Bravely_Default_II'] + tmp
-        comDir = '/'.join(tmp)
         if not comDir:
             return self.baseDir, comDir
         if comDir[-1] != '/':
@@ -185,13 +181,8 @@ class ROM:
                 continue
             base = len(pakData)
             # Filename (relative to the new base directory)
-            ## TEMPORARY
-            k = key.split('/')[1:]
-            k = ['Bravely_Default_II'] + k
-            k = '/'.join(k)
             if comDir:
-                # tmpDir = key.split(comDir)[-1]
-                tmpDir = k.split(comDir)[-1]
+                tmpDir = key.split(comDir)[-1]
             else:
                 # tmpDir = key
                 tmpDir = k
